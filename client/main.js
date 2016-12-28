bca9e72a973b560bedfd5be8eefa717f265954a7
andela-bmakinwa <makinwa37@gmail.com>
2016-12-28 06:49:04 +0100

    Build with blaze framework

---
+++ client/main.js
@@ -0,0 +1,4 @@
+/* jshint esversion: 6 */
+
+import '../imports/startup/accounts-config.js';
+import '../imports/ui/body.js';
---
+++ imports/ui/body.js
@@ -0,0 +1,46 @@
+/* jshint esversion: 6 */
+
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+import { Tasks } from '../api/tasks.js';
+
+import './task.js';
+import './body.html';
+
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+  Meteor.subscribe('tasks');
+});
+
+Template.body.helpers({
+  tasks() {
+    const instance = Template.instance();
+    if(instance.state.get('hideCompleted')) {
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  }
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    Meteor.call('tasks.insert', text);
+
+    target.text.value = '';
+  },
+
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
+  }
+});
